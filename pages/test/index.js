d045ef29d555a6b59c4cdd645397bdefcbc854de
tochibedford <musicofbyte@gmail.com>
2022-08-30 19:26:23 +0100

    path alias set up and testing route for components

---
+++ pages/test/index.js
@@ -0,0 +1,14 @@
+import Button from "../../components/buttons/button";
+
+// use this file to test components
+const Test = () => {
+    return ( 
+        /* Start Code Here */
+        <>
+            <Button buttonType='startWorkoutButton'>Hellooo</Button>  
+        </>
+        /* End Code Here */
+     );
+}
+ 
+export default Test;
