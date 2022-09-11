93372575650d55b3cfb85d27416a454a04ce3a58
Martins Ikpe <martinsikpe25@gmail.com>
2022-09-11 23:46:57 +0100

    Created the options modal

---
+++ components/modals/Options.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from "@styles/Modal.module.scss"
+const Options = ({ options }) => {
+    //Takes in a prop of an array and displays the array contents
+    return (
+        <div>options
+            <div className={`${styles.optionsModal}`}>
+                {
+                    options.map((option, index) => {
+                        return (
+                            <h1 className={`${styles.optionsModalItems}`} key={index}>{option.name}</h1>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Options
