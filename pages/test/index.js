f0db8d04ff90f14894c7c46421ba3bf9ad54b005
tochibedford <musicofbyte@gmail.com>
2022-09-12 10:14:57 +0100

    modified test page style

--- pages/test/index.js
+++ pages/test/index.js
@@ -1,3 +1,4 @@
+import styles from '@styles/test.module.scss';
 import SearchBar from "@components/searchbar/SearchBar";
 import Button from "../../components/buttons/button";
 import Options from '@components/modals/Options'
@@ -17,57 +18,62 @@ const Test = () => {
     const a = [{ name: "Edit" }, { name: "Delete" }, { name: "Force Finish" }];
     return (
         /* Start Code Here */
-        <>
+        <div className={styles.body}>
             <h1>
                 This is the test Route, Place components here to check them out visually : )
             </h1>
-            <br />
-            <br />
-            <h2>
-                Buttons
-            </h2>
-            <Button buttonType='startWorkoutButton'>Start Workout Plan</Button>
-            <Button buttonType='finishWorkoutButton'>Set Finish</Button>
-            <Button buttonType='createWorkoutButton'>Create Workout Plan Now</Button>
-            <Button buttonType='filterButton'>Filter</Button>
-            <Button buttonType='addPlanButton'></Button>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
 
-            <h2>
-                Search Bar
-            </h2>
-            <SearchBar />
+            <div className={styles.componentGroup}>
+                <h2>
+                    Buttons
+                </h2>
+                <Button buttonType='startWorkoutButton'>Start Workout Plan</Button>
+                <Button buttonType='finishWorkoutButton'>Set Finish</Button>
+                <Button buttonType='createWorkoutButton'>Create Workout Plan Now</Button>
+                <Button buttonType='filterButton'>Filter</Button>
+                <Button buttonType='addPlanButton'></Button>
+            </div>
 
-            <h2>
-                Options
-            </h2>
-            <Options options={a}></Options>
-            <h2>
-                Avatar
-            </h2>
-            <Avatar name={user.name} status={user.status} image={user.image}></Avatar>
-            <h2>
-                DateSummary
-            </h2>
-            <DateSummary day={4} weekday="Mon"></DateSummary>
-            <h2>
-                MiniDateSummary
-            </h2>
-            <MiniDateSummary day={4}></MiniDateSummary>
+            <div className={styles.componentGroup}>
+                <h2>
+                    Search Bar
+                </h2>
+                <SearchBar />
+            </div>
 
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
+            <div className={styles.componentGroup}>
+                <h2>
+                    Options
+                </h2>
+                <Options options={a}></Options>
+            </div>
 
-            <h2>Notifications</h2>
-            <Notifications notifications={["1"]} messages={["hey"]} gifts={["lollipop"]}/>
-        </>
+            <div className={styles.componentGroup}>
+                <h2>
+                    Avatar
+                </h2>
+                <Avatar name={user.name} status={user.status} image={user.image}></Avatar>
+            </div>
+
+            <div className={styles.componentGroup}>
+                <h2>
+                    DateSummary
+                </h2>
+                <DateSummary day={4} weekday="Mon"></DateSummary>
+            </div>
+
+            <div className={styles.componentGroup}>
+                <h2>
+                    MiniDateSummary
+                </h2>
+                <MiniDateSummary day={4}></MiniDateSummary>
+            </div>
+
+            <div className={styles.componentGroup}>
+                <h2>Notifications</h2>
+                <Notifications notifications={["1"]} messages={["hey"]} gifts={["lollipop"]}/>
+            </div>
+        </div>
         /* End Code Here */
     );
 }
