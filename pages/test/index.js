7f0f722d73573b46d1631283aecb86b310a8e2f5
tochibedford <musicofbyte@gmail.com>
2022-09-12 10:37:36 +0100

    styled the test page and added authors

--- pages/test/index.js
+++ pages/test/index.js
@@ -18,60 +18,64 @@ const Test = () => {
     const a = [{ name: "Edit" }, { name: "Delete" }, { name: "Force Finish" }];
     return (
         /* Start Code Here */
-        <div className={styles.body}>
-            <h1>
-                This is the test Route, Place components here to check them out visually : )
-            </h1>
+        <div className={styles.bodyContainer}>
+            <div className={styles.body}>
+                <h1>
+                    This is the test Route, Place components here to check them out visually : )
+                </h1>
 
-            <div className={styles.componentGroup}>
-                <h2>
-                    Buttons
-                </h2>
-                <Button buttonType='startWorkoutButton'>Start Workout Plan</Button>
-                <Button buttonType='finishWorkoutButton'>Set Finish</Button>
-                <Button buttonType='createWorkoutButton'>Create Workout Plan Now</Button>
-                <Button buttonType='filterButton'>Filter</Button>
-                <Button buttonType='addPlanButton'></Button>
-            </div>
+                <div className={styles.componentGroup}>
+                    <h2 className={styles.groupName}>
+                        Buttons - <i>Tochi</i>
+                    </h2>
+                    <Button buttonType='startWorkoutButton'>Start Workout Plan</Button>
+                    <Button buttonType='finishWorkoutButton'>Set Finish</Button>
+                    <Button buttonType='createWorkoutButton'>Create Workout Plan Now</Button>
+                    <Button buttonType='filterButton'>Filter</Button>
+                    <Button buttonType='addPlanButton'></Button>
+                </div>
 
-            <div className={styles.componentGroup}>
-                <h2>
-                    Search Bar
-                </h2>
-                <SearchBar />
-            </div>
+                <div className={styles.componentGroup}>
+                    <h2 className={styles.groupName}>
+                        Search Bar - <i>Tochi</i>
+                    </h2>
+                    <SearchBar />
+                </div>
 
-            <div className={styles.componentGroup}>
-                <h2>
-                    Options
-                </h2>
-                <Options options={a}></Options>
-            </div>
+                <div className={styles.componentGroup}>
+                    <h2 className={styles.groupName}>
+                        Options - <i>Martins</i>
+                    </h2>
+                    <Options options={a}></Options>
+                </div>
 
-            <div className={styles.componentGroup}>
-                <h2>
-                    Avatar
-                </h2>
-                <Avatar name={user.name} status={user.status} image={user.image}></Avatar>
-            </div>
+                <div className={styles.componentGroup}>
+                    <h2 className={styles.groupName}>
+                        Avatar - <i>Martins</i>
+                    </h2>
+                    <Avatar name={user.name} status={user.status} image={user.image}></Avatar>
+                </div>
 
-            <div className={styles.componentGroup}>
-                <h2>
-                    DateSummary
-                </h2>
-                <DateSummary day={4} weekday="Mon"></DateSummary>
-            </div>
+                <div className={styles.componentGroup}>
+                    <h2 className={styles.groupName}>
+                        DateSummary - <i>Martins</i>
+                    </h2>
+                    <DateSummary day={4} weekday="Mon"></DateSummary>
+                </div>
 
-            <div className={styles.componentGroup}>
-                <h2>
-                    MiniDateSummary
-                </h2>
-                <MiniDateSummary day={4}></MiniDateSummary>
-            </div>
+                <div className={styles.componentGroup}>
+                    <h2 className={styles.groupName}>
+                        MiniDateSummary - <i>Martins</i>
+                    </h2>
+                    <MiniDateSummary day={4}></MiniDateSummary>
+                </div>
 
-            <div className={styles.componentGroup}>
-                <h2>Notifications</h2>
-                <Notifications notifications={["1"]} messages={["hey"]} gifts={["lollipop"]}/>
+                <div className={styles.componentGroup}>
+                    <h2 className={styles.groupName}>
+                        Notifications - <i>Tochi</i>
+                    </h2>
+                    <Notifications notifications={["1"]} messages={["hey"]} gifts={["lollipop"]}/>
+                </div>
             </div>
         </div>
         /* End Code Here */
