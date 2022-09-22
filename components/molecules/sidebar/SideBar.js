c28d392c9ce14d1c20ee7d5a3f46cdb450e7f925
tochibedford <musicofbyte@gmail.com>
2022-09-22 10:32:26 +0100

    implementing selector and active nav

--- components/molecules/sidebar/SideBar.js
+++ components/molecules/sidebar/SideBar.js
@@ -2,20 +2,39 @@ import styles from "@styles/SideBar.module.scss"
 import Image from "next/image"
 import Button from "@components/atoms/buttons/Button"
 
+// icons
 import DashboardIcon from '@icons/DashboardIcon.svg'
 import WStatisticIcon from '@icons/WStatisticIcon.svg'
 import WPlanIcon from '@icons/WPlanIcon.svg'
 import DistanceMapIcon from '@icons/DistanceMapIcon.svg'
 import DietMoveMenuIcon from '@icons/DietMoveMenuIcon.svg'
 import PersonalBrandIcon from '@icons/PersonalBrandIcon.svg'
+import { useEffect, useRef } from "react"
 
 
 const SideBar = () => {
+    const menuElement = useRef(null)
+    const menuSelector = useRef(null)
+
+    useEffect(()=>{
+        const menuEl = menuElement.current // .current may change by the time clean up runs, so this is better
+        const handleMouseover = (e)=>{
+            const currentTargetRect = e.currentTarget.getBoundingClientRect()
+            const offsetYFromMenu = e.pageY - currentTargetRect.top
+            const menuItemHeight = menuSelector.current.getBoundingClientRect().height
+            menuSelector.current.style.top = `${menuItemHeight*Math.floor(offsetYFromMenu/menuItemHeight)}px`
+        }
+        menuEl.addEventListener("mouseover", handleMouseover, {passive: true})
+        
+        return ()=>{
+            menuEl.removeEventListener("mouseover", handleMouseover, {passive: true})
+        }
+    }, [])
     return ( 
         <div className={styles.container}>
             <div className={styles.title}>Main Menu</div>
-            <ul className={styles.menu}>
-                <div className={styles.menuSelector}></div>
+            <ul className={styles.menu} ref={menuElement}>
+                <div className={styles.menuSelector} ref={menuSelector}></div>
                 <li className={styles.menuItem}>
                     <div>
                         <Image src={DashboardIcon} alt="dashboard icon"/>
