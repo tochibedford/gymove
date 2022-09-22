97f35277213f73d54636e67bef7821cb9b8a4b10
tochibedford <musicofbyte@gmail.com>
2022-09-22 12:57:20 +0100

    feat: implemented sidebar molecule

--- components/molecules/sidebar/SideBar.js
+++ components/molecules/sidebar/SideBar.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react"
 import styles from "@styles/SideBar.module.scss"
 import Image from "next/image"
 import Button from "@components/atoms/buttons/Button"
@@ -9,33 +10,67 @@ import WPlanIcon from '@icons/WPlanIcon.svg'
 import DistanceMapIcon from '@icons/DistanceMapIcon.svg'
 import DietMoveMenuIcon from '@icons/DietMoveMenuIcon.svg'
 import PersonalBrandIcon from '@icons/PersonalBrandIcon.svg'
-import { useEffect, useRef } from "react"
 
 
 const SideBar = () => {
+    const [activeMenuItem, setActiveMenuItem] = useState(3) //maybe save this to local storage to improve resumability?
     const menuElement = useRef(null)
     const menuSelector = useRef(null)
+    const activeMenuItemRef = useRef(activeMenuItem)
 
     useEffect(()=>{
+        const menuItemHeightCss = parseFloat(styles.menuItemHeight)*parseFloat(getComputedStyle(document.documentElement).fontSize) //computing the menu item height using the value from the css variable and getComputedStyle to get the value of 1rem
+        menuSelector.current.style.top = `${menuItemHeightCss*activeMenuItem}px`
         const menuEl = menuElement.current // .current may change by the time clean up runs, so this is better
-        const handleMouseover = (e)=>{
+        const activeMenuElement = menuEl.querySelector(`[item-number='${activeMenuItem}']`)
+        activeMenuElement.classList.add(styles.activeMenuItem)
+
+        const handleMouseClick = (e)=>{
+            let activeMenuElement = menuEl.querySelector(`[item-number='${activeMenuItem}']`)
+            activeMenuElement.classList.remove(styles.activeMenuItem)
+            
+            setActiveMenuItem(()=>{
+                let newValue;
+                if(e.target.tagName === 'DIV'){
+                    newValue = parseInt(e.target.parentNode.getAttribute("item-number"))
+                }else{
+                    newValue = parseInt(e.target.getAttribute("item-number"))
+                }
+                activeMenuElement = menuEl.querySelector(`[item-number='${newValue}']`)
+                activeMenuElement.classList.add(styles.activeMenuItem)
+                return newValue
+            })
+        }
+
+        const handleMouseOver = (e)=>{
             const currentTargetRect = e.currentTarget.getBoundingClientRect()
             const offsetYFromMenu = e.pageY - currentTargetRect.top
             const menuItemHeight = menuSelector.current.getBoundingClientRect().height
             menuSelector.current.style.top = `${menuItemHeight*Math.floor(offsetYFromMenu/menuItemHeight)}px`
         }
-        menuEl.addEventListener("mouseover", handleMouseover, {passive: true})
-        
-        return ()=>{
-            menuEl.removeEventListener("mouseover", handleMouseover, {passive: true})
+
+        const handleMouseOut = ()=>{
+            const menuItemHeightCss = parseFloat(styles.menuItemHeight)*parseFloat(getComputedStyle(document.documentElement).fontSize)
+            menuSelector.current.style.top = `${menuItemHeightCss*activeMenuItem}px`
         }
-    }, [])
+
+        menuEl.addEventListener("click", handleMouseClick, {passive: true})
+        menuEl.addEventListener("mouseover", handleMouseOver, {passive: true})
+        menuEl.addEventListener("mouseout", handleMouseOut, {passive: true})
+
+        return ()=>{
+            menuEl.removeEventListener("click", handleMouseClick, {passive: true})
+            menuEl.removeEventListener("mouseover", handleMouseOver, {passive: true})
+            menuEl.removeEventListener("mouseout", handleMouseOut, {passive: true})
+        }
+    }, [activeMenuItem])
+
     return ( 
         <div className={styles.container}>
             <div className={styles.title}>Main Menu</div>
             <ul className={styles.menu} ref={menuElement}>
                 <div className={styles.menuSelector} ref={menuSelector}></div>
-                <li className={styles.menuItem}>
+                <li className={styles.menuItem} item-number={0}>
                     <div>
                         <Image src={DashboardIcon} alt="dashboard icon"/>
                     </div>
@@ -43,7 +78,7 @@ const SideBar = () => {
                         Dashboard
                     </div>
                 </li>
-                <li className={styles.menuItem}>
+                <li className={styles.menuItem} item-number={1}>
                     <div>
                         <Image src={WStatisticIcon} alt="dashboard icon"/>
                     </div>
@@ -51,7 +86,7 @@ const SideBar = () => {
                         Workout Statistic
                     </div>
                 </li>
-                <li className={styles.menuItem}>
+                <li className={styles.menuItem} item-number={2}>
                     <div>
                         <Image src={WPlanIcon} alt="dashboard icon"/>
                     </div>
@@ -59,7 +94,7 @@ const SideBar = () => {
                         Workout Plan
                     </div>
                 </li>
-                <li className={styles.menuItem}>
+                <li className={styles.menuItem} item-number={3}>
                     <div>
                         <Image src={DistanceMapIcon} alt="dashboard icon"/>
                     </div>
@@ -67,20 +102,20 @@ const SideBar = () => {
                         Distance Map
                     </div>
                 </li>
-                <li className={styles.menuItem}>
+                <li className={styles.menuItem} item-number={4}>
                     <div>
                         <Image src={DietMoveMenuIcon} alt="dashboard icon"/>
                     </div>
                     <div>
-                        Distance Map
+                        Diet Move Menu
                     </div>
                 </li>
-                <li className={styles.menuItem}>
+                <li className={styles.menuItem} item-number={5}>
                     <div>
                         <Image src={PersonalBrandIcon} alt="dashboard icon"/>
                     </div>
                     <div>
-                        Distance Map
+                        Personal Brand
                     </div>
                 </li>
             </ul>
@@ -93,7 +128,7 @@ const SideBar = () => {
                 <div className={styles.madeBy}>Made with ♥ by Tochi & Martins</div>
             </div>
         </div>
-     );
+     )
 }
  
-export default SideBar;
+export default SideBar
